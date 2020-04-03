19578cf0524185b2899c941a9ed8dee22b00e44f
Michael Pohl <michael.pohl@bahn-x.de>
2020-04-03 09:08:25 +0200

    Move from arrays of adapters to vararg

--- jsondataclassmodule/src/main/java/com/michaelpohl/jsondataclassmodule/JsonDataClass.kt
+++ jsondataclassmodule/src/main/java/com/michaelpohl/jsondataclassmodule/JsonDataClass.kt
@@ -2,14 +2,16 @@ package com.michaelpohl.jsondataclassmodule
 
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import java.lang.reflect.Type
+import java.util.*
 
 /**
  * All sub classes must be data classes!
  */
 open class JsonDataClass {
-    fun toJsonString(adapters: Array<out AdapterDefinition>? = adapterDefinitions): String {
+    fun toJsonString(vararg adapters: AdapterDefinition): String {
         return buildMoshi(adapters).adapter(this.javaClass).toJson(this)
     }
 
@@ -19,7 +21,7 @@ open class JsonDataClass {
 
         inline fun <reified T : JsonDataClass> fromJsonString(
             string: String,
-            adapters: Array<out AdapterDefinition>? = adapterDefinitions
+            vararg adapters: AdapterDefinition
         ): T? {
             return buildMoshi(adapters).adapter(T::class.java).fromJson(string)
         }
@@ -27,10 +29,10 @@ open class JsonDataClass {
         fun buildMoshi(adapters: Array<out AdapterDefinition>? = adapterDefinitions): Moshi {
             val builder = Moshi.Builder()
 
-            adapters?.let {
-                if (it.isNotEmpty()) {
+            adapters ?: adapterDefinitions.apply {
+                if (this.isNotEmpty()) {
                     builder.add(KotlinJsonAdapterFactory())
-                    it.forEach { definition ->
+                    this.forEach { definition ->
                         builder.add(definition.first, definition.second)
                     }
                 }
